997e1ea7141d8a37354903e837a2d3bd1b4e4c1c
dasaaf <as.f09942@gmail.com>
2021-11-21 16:06:09 -0500

    The backend has been created with strapi

---
+++ smart-cities-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9642eddd6422677343a07396fabd2748'),
+    },
+  },
+});
