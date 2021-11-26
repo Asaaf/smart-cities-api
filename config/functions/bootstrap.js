930cd1437bc50d0d0dab9c0f2e9ccb884b548f4c
Asaaf <as_f0994@hotmail.com>
2021-11-25 23:42:32 -0500

    config knex setup

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,4 +10,10 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+    const knex = strapi.connections.default;
+    await knex.schema.alterTable('provinces', t => {
+        console.log('provinces change', t)
+        //t.integer('country').unsigned().notNullable().references('countries.id').onDelete('cascade').alter();
+    });
+  }
