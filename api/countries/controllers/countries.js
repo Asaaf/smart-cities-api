1fda22b1b267a4b0b91fcd7ff117fa51797728d8
Asaaf <as_f0994@hotmail.com>
2021-11-29 19:18:53 -0500

    use relation and prevent autoPopulate

--- api/countries/controllers/countries.js
+++ api/countries/controllers/countries.js
@@ -1,13 +1,25 @@
 'use strict';
 
+const { sanitizeEntity } = require('strapi-utils');
+const { _ } = require('lodash');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-find: async (ctx, next) => {
-  console.log('/suggested-users accessed.');
-  await next();
-}
+module.exports = { 
 
-module.exports = {};
+  find: async(ctx) => {
+    const countries = await strapi.query('countries').model.fetchAll({
+      columns: ['id', 'name'],
+    });
+    return countries;
+  },
+
+  findOne: async(ctx) => {
+    const { id } = ctx.params;
+    const country = await strapi.query('countries').findOne({ id }, [ 'provinces' ]);
+    return country;
+  }
+};
