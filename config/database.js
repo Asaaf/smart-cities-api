da390cd178ef5eb72dce399f7df0e18155ad2418
Asaaf <as_f0994@hotmail.com>
2021-12-16 21:48:09 -0500

    database remove .env values

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'smartcities_backend_db'),
-        username: env('DATABASE_USERNAME', 'hummingbird'),
-        password: env('DATABASE_PASSWORD', 'X6QtKjV7DqjxGFSg'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {
