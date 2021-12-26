a72fe15689f956608a3634ee8e17c9b3b4376527
Asaaf <as_f0994@hotmail.com>
2021-12-25 19:37:22 -0500

    add /activites/tourists endpoint for report

--- api/activities/controllers/activities.js
+++ api/activities/controllers/activities.js
@@ -5,4 +5,14 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async tourists (ctx) {
+        const resulset = await strapi.connections.default.raw(
+            `SELECT COUNT(ta.activity_id) AS total, a.id, a.name
+            FROM activities as a
+            INNER JOIN tourist_activities ta on a.id = ta.activity_id
+            GROUP BY a.id`
+        );
+        return resulset[0];
+    }
+};
