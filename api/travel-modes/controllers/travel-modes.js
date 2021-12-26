e7ec7f3ee5c65320687cf6abaf428de3970b51df
Asaaf <as_f0994@hotmail.com>
2021-12-25 20:18:26 -0500

    add /travel-modes/tourists endpoint for report

--- api/travel-modes/controllers/travel-modes.js
+++ api/travel-modes/controllers/travel-modes.js
@@ -5,4 +5,14 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async tourists (ctx) {
+        const resulset = await strapi.connections.default.raw(
+            `SELECT COUNT(v.travel_mode_id) AS total, tm.id, tm.name
+            FROM travel_modes as tm
+            INNER JOIN visits v ON tm.id = v.travel_mode_id
+            GROUP BY tm.id;`
+        );
+        return resulset[0];
+    }
+};
